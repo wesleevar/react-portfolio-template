f1ad162913ba0b53770320f722b57f0d2a82d4fd
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-03-24 14:07:26 -0400

    componentized Connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Connect from "./components/Connect";
 import Navbar from "./components/Navbar";
 import Profile from "./components/Profile";
 import { fadeInTableRows } from "./utils/fadeInTable";
@@ -270,40 +271,8 @@ function App() {
         <div className="content-separator"></div>
 
         {/* connect Section */}
-        {/* TODO: with your info --> */}
-        <h2 id="connect">Let's Connect</h2>
-        <div className="connect-container">
-          <p>Feel free to reach out through any of these platforms:</p>
-          <ul className="connect-links">
-            <li>
-              <a
-                href="mailto:wderocco@bu.edu"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                📧 Email
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/wderocco8"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                💻 GitHub
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://linkedin.com/in/william-derocco"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                💼 LinkedIn
-              </a>
-            </li>
-          </ul>
-        </div>
+        {/* TODO: navigate to `components/Connect.tsx and update all TODO:s */}
+        <Connect />
       </div>
     </>
   );
---
+++ src/components/Connect.tsx
@@ -0,0 +1,40 @@
+export default function Connect() {
+  return (
+    <>
+      {/* TODO: with your info --> */}
+      <h2 id="connect">Let's Connect</h2>
+      <div className="connect-container">
+        <p>Feel free to reach out through any of these platforms:</p>
+        <ul className="connect-links">
+          <li>
+            <a
+              href="mailto:wderocco@bu.edu"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              📧 Email
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/wderocco8"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              💻 GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://linkedin.com/in/william-derocco"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              💼 LinkedIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+}
