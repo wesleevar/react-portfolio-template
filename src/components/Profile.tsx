31b3e6196c383d3d9ab81d74815d0f8892d44e1e
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2025-03-24 16:11:00 -0400

    abstracted profile page

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,18 +1,38 @@
 import Typewriter from "typewriter-effect";
 
+interface ProfileData {
+  typewriterName: string[];
+  graduatingClass: number;
+  majorMinor: string;
+  currentPosition?: string;
+  websiteHref: string;
+  imgSrc: string;
+}
+
+/**
+ * TODO: replace each `typewriterName`, `graduatingClass`, `majorMinor`,
+ * `websiteHref`, and `imgSrc` with your personal information.
+ */
+const profileData: ProfileData = {
+  typewriterName: [
+    "Heyo peeps, I'm Will.",
+    "I love to learn.",
+    "William De Rocco :)",
+  ],
+  graduatingClass: 2025,
+  majorMinor: "B.A. in Computer Science, Minor in Economics",
+  websiteHref: "https://github.com/wderocco8",
+  imgSrc: "/images/profile/profile.jpeg",
+};
+
 export default function Profile() {
   return (
     <div className="profile-flex">
       <div>
         <h1>
-          {/* TODO: replace `strings` with custom phrases (lines 39-41) --> */}
           <Typewriter
             options={{
-              strings: [
-                "Heyo peeps, I'm Will.",
-                "I love to learn.",
-                "William De Rocco :)",
-              ],
+              strings: profileData.typewriterName,
               autoStart: true,
               loop: true,
               delay: 75,
@@ -20,24 +40,24 @@ export default function Profile() {
           />
         </h1>
         <ul>
-          {/* TODO: with your info --> */}
-          <li>Class of 2025</li>
-          <li>B.A. in Computer Science, Minor in Economics</li>
-          <li>Teaching Assistant for cs412 (Full-Stack Applications)</li>
+          <li>Class of {profileData.graduatingClass}</li>
+          <li>{profileData.majorMinor}</li>
+          {profileData.currentPosition && (
+            <li>{profileData.currentPosition}</li>
+          )}
         </ul>
       </div>
 
-      {/* TODO: with your info --> */}
       <div className="profile tilt">
         <a
-          href="https://github.com/wderocco8"
+          href={profileData.websiteHref}
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
             className="profile-img"
-            src="/images/profile/profile.jpeg"
-            alt="profile"
+            src={profileData.imgSrc}
+            alt="Profile picture."
           />
         </a>
       </div>
